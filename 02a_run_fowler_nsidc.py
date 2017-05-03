dc9d967d0bcf1d1a87106981d69afab9fef9b918
akorosov <korosov@gmail.com>
2017-05-03 14:52:12 +0200

    add 02b

--- 02a_run_fowler_nsidc.py
+++ 02a_run_fowler_nsidc.py
@@ -11,16 +11,15 @@ from iceagelib import *
 #ftp://osisaf.met.no/archive_test/ice/drift_lr/osi405c_demo_archive/2013/01/ice_drift_nh_polstere-625_multi-oi_201301191200-201301211200.nc
 #wget -w 1 -r -nc -nd -A 'ice_drift_nh_*.nc' -P /Data/sat/downloads/osi405c_demo_archive/ ftp://osisaf.met.no/archive_test/ice/drift_lr/osi405c_demo_archive/2016
 
-idir_uv = '/files/nsidc0116_icemotion_vectors_v3/'
-idir_ia = '/files/nsidc0611_seaice_age_v3/'
-
 ### NSIDC U/V
+nsidc_sid_dir = '/files/nsidc0116_icemotion_vectors_v3/'
+
 reader = get_nsidc_uv
 get_date = get_nsidc_date
 src_res = 25000 # spatial resolution of the source datasets (m)
 h = 7 * 24 * 60 * 60 # temoral resolution (sec)
 
-nsidc_sid_files = sorted(glob.glob(idir_uv + 'icemotion.grid.week*.bin'))
+nsidc_sid_files = sorted(glob.glob(nsidc_sid_dir + 'icemotion.grid.week*.bin'))
 factors = [2, 4, 8]
 odirmsk = 'nsidc_1985_f%02d/'
 
