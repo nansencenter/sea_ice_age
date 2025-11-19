85993cdcf4bd91cc5d8afc2b3c14af376d90d0c9
Anton Korosov <korosov@gmail.com>
2025-11-19 16:30:02 +0100

    Add integarted uncertainty

--- lmsiage/uncertainty.py
+++ lmsiage/uncertainty.py
@@ -73,55 +73,26 @@ class ComputeObsUncertainty:
                 }, mode='a')
 
 
-class ComputeSicUncertainty:
-    def __init__(self, sid_files, sid_dates, mesh_dir, age_dir, unc_dir, n_steps, xc, yc):
-        self.sid_files = sid_files
-        self.sid_dates = sid_dates
+class ComputeIntegratedUncertainty:
+    def __init__(self, mesh_dir, age_dir, unc_dir, n_steps):
         self.n_steps = n_steps
         self.mesh_dir = mesh_dir
         self.age_dir = age_dir
         self.unc_dir = unc_dir
-        self.xc = xc
-        self.yc = yc
 
-    def load_sic_data(self, sid_file, mesh_file):
-        with np.load(sid_file) as d:
-            sic_unc = d['sic_unc']
-        
-        mf = MeshFile(mesh_file)
-        x, y, t = mf.load(['x', 'y', 't'], as_dict=False)
-        return sic_unc, x, y, t
-
-    def compute_obs_uncertainty(self, year):
-        unc_sic_name = 'unc_sic'
-        mesh_files = sorted(glob.glob(f'{self.mesh_dir}/{year}/mesh_*.zip'))
-        mesh_dates = [datetime.strptime(os.path.basename(mesh_file), 'mesh_%Y%m%d.zip') + timedelta(days=0.5) for mesh_file in mesh_files]
-        unc_files = [mesh_date.strftime(f'{self.unc_dir}/%Y/unc_%Y%m%d.zip') for mesh_date in mesh_dates]
-        print(f'Computing SIC uncertainty for {len(mesh_files)} mesh files for year {year}')
-        dst_dir = f'{self.unc_dir}/{year}'
-        os.makedirs(dst_dir, exist_ok=True)
-        for mesh_file, mesh_date, unc_file in zip(tqdm(mesh_files), mesh_dates, unc_files):
-            if unc_sic_name in get_file_arrays(unc_file):
-                continue
-            
-            sid_file = self.sid_files[self.sid_dates.index(mesh_date)]
-            # load SIC uncert from obs file
-            unc_sic_grd, x, y, t = self.load_sic_data(sid_file, mesh_file)
-            unc_sic_fil = fill_gaps(unc_sic_grd, np.isnan(unc_sic_grd), distance=144)
-            # interpolate uncertainty to mesh
-            unc_sic = RectBivariateSpline(self.xc, self.yc, unc_sic_fil[::-1], kx=1, ky=1)(y[t].mean(axis=1), x[t].mean(axis=1), grid=False)
-            # Save uncertainty
-            unc_mf = MeshFile(unc_file)
-            unc_mf.save({unc_sic_name: unc_sic.astype(np.float16)}, mode='a')
-
-    def compute_min_uncertainty(self, unc_sic_dates, start_idx):
-        stop_idx = min(start_idx + self.n_steps, len(unc_sic_dates))
+    def compute(self, unc_sic_dates, start_idx):
+        # init integrated uncerts
         sic_min = None
         unc_sic_min = None
+        unc_sid_int = None
+
+        stop_idx = min(start_idx + self.n_steps, len(unc_sic_dates))
         start_date = unc_sic_dates[start_idx]
+        
         unc_sic_name = f'unc_sic{start_date.year}'
         sic_min_name = f'sic_min{start_date.year}'
         sic_name = f'sic{start_date.strftime("%Y%m%d")}'
+        unc_sid_name = f'unc_sid{start_date.year}'
 
         print(f'Computing uncertainty for {start_date.strftime("%Y%m%d")}, {unc_sic_dates[stop_idx-1]}')
         for i in trange(start_idx, stop_idx):
@@ -130,24 +101,30 @@ class ComputeSicUncertainty:
             age_file = unc_date.strftime(f'{self.age_dir}/%Y/age_%Y%m%d.zip')
             unc_file = unc_date.strftime(f'{self.unc_dir}/%Y/unc_%Y%m%d.zip')
             # load precomputed min concentration and its uncertainty, skip the rest of processing
-            if unc_sic_name in get_file_arrays(unc_file):
-                unc_sic_min, sic_min = MeshFile(unc_file).load([unc_sic_name, sic_min_name], as_dict=False)
+            file_arrays = get_file_arrays(unc_file)
+            if unc_sic_name in file_arrays and unc_sid_name in file_arrays:
+                sic_min, unc_sic_min, unc_sid_int = MeshFile(unc_file).load([sic_min_name, unc_sic_name, unc_sid_name], as_dict=False)
                 continue
 
             # load obs concentration, uncertainty
             sic = MeshFile(age_file).load([sic_name], as_dict=False)[0]
-            unc_sic = MeshFile(unc_file).load(['unc_sic'], as_dict=False)[0]
+            unc_sic, unc_sid = MeshFile(unc_file).load(['unc_sic', 'unc_sid'], as_dict=False)
             if unc_sic_min is None:
                 # first time step: initialize minimal concentration and its uncertainty
                 unc_sic_min = np.array(unc_sic)
                 sic_min = np.array(sic)
+                # initialize drift integrated uncertainty
+                unc_sid_int = np.array(unc_sid)
             else:
                 # later steps: advect sic_min, unc_sic_min from previous time step
                 src2dst, weights = MeshFile(mesh_file).load(['src2dst', 'weights'], as_dict=False)
+
                 unc_sic_min_advected = np.zeros(src2dst[:,1].max()+1)
                 np.add.at(unc_sic_min_advected, src2dst[:,1], unc_sic_min[src2dst[:,0]] * weights)
                 sic_min_advected = np.zeros(src2dst[:,1].max()+1)
                 np.add.at(sic_min_advected, src2dst[:,1], sic_min[src2dst[:,0]] * weights)
+                unc_sid_int_advected = np.zeros(src2dst[:,1].max()+1)
+                np.add.at(unc_sid_int_advected, src2dst[:,1], unc_sid_int[src2dst[:,0]] * weights)
 
                 # keep minimal value of SIC
                 sic_min = np.min([sic_min_advected, sic], axis=0)
@@ -156,94 +133,17 @@ class ComputeSicUncertainty:
                 min_sic_ids = sic < sic_min_advected
                 unc_sic_min = unc_sic_min_advected
                 unc_sic_min[min_sic_ids] = unc_sic[min_sic_ids]
+
+                # compute new drift integrated uncertainty after advection
+                unc_sid_int = np.hypot(unc_sid, unc_sid_int_advected)
             
             # Save minimal concentration and its uncertainty
             MeshFile(unc_file).save({
                     sic_min_name: sic_min.astype(np.float16),
                     unc_sic_name: unc_sic_min.astype(np.float16),
+                    unc_sid_name: unc_sid_int.astype(np.float16),
                  }, mode='a')
         print('Done', start_date, unc_sic_dates[stop_idx-1])
-
-
-class ComputeSidUncertainty:
-    def __init__(self, mesh_dir, unc_dir, ifiles, idates, n_steps, size, xc, yc):
-        self.mesh_dir = mesh_dir
-        self.unc_dir = unc_dir
-        self.ifiles = ifiles
-        self.idates = idates
-        self.n_steps = n_steps
-        self.size = size
-        self.xc = xc
-        self.yc = yc
-
-    def load_sid_data(self, ifile, mesh_file):
-        with np.load(ifile) as d:
-            sid_unc = d['sid_unc']
-        
-        mf = MeshFile(mesh_file)
-        x, y, t = mf.load(['x', 'y', 't'], as_dict=False)
-        return sid_unc, x, y, t
-    
-    def compute_smearing_uncertainty(self, year):
-        """ Compute smearing uncertainty and integrated drift uncertainty """
-        unc_sid_name = 'unc_sid'
-        mesh_files = sorted(glob.glob(f'{self.mesh_dir}/{year}/mesh_*.zip'))
-        mesh_dates = [datetime.strptime(os.path.basename(mesh_file), 'mesh_%Y%m%d.zip') + timedelta(days=0.5) for mesh_file in mesh_files]
-        unc_files = [mesh_date.strftime(f'{self.unc_dir}/%Y/unc_%Y%m%d.zip') for mesh_date in mesh_dates]
-            
-        dst_dir = f'{self.unc_dir}/{year}'
-        os.makedirs(dst_dir, exist_ok=True)
-
-        for mesh_file, mesh_date, unc_file in zip(mesh_files, mesh_dates, unc_files):
-            if unc_sid_name in get_file_arrays(unc_file):
-                continue
-
-            ifile = self.ifiles[self.idates.index(mesh_date)]
-            unc_sid_grd, x, y, t = self.load_sid_data(ifile, mesh_file)
-            unc_sid_fil = fill_gaps(unc_sid_grd, np.isnan(unc_sid_grd), distance=144)
-            # compute uncertainty of drift field after smoothing
-            unc_sid_smt = uniform_filter(unc_sid_fil**2, size=self.size) / self.size
-            unc_sid_smt[np.isnan(unc_sid_grd)] = 0
-            # interpolate uncertainty to mesh
-            unc_sid = RectBivariateSpline(self.xc[1:-1:3], self.yc[1:-1:3], unc_sid_smt[::-1], kx=1, ky=1)(y[t].mean(axis=1), x[t].mean(axis=1), grid=False)
-            # Save uncertainty
-            unc_mf = MeshFile(unc_file)
-            unc_mf.save({unc_sid_name: unc_sid.astype(np.float16)}, mode='a')
-
-    def compute_integrated_uncertainty(self, unc_sid_dates, start_idx):
-        """ Compute integrated drift uncertainty """
-        stop_idx = min(start_idx + self.n_steps, len(unc_sid_dates))
-        unc_sid_int = None
-        start_date = unc_sid_dates[start_idx]
-        unc_sid_name = f'unc_sid{start_date.year}'
-        print('Processing', start_date, unc_sid_dates[stop_idx-1])
-        for i in range(start_idx, stop_idx):
-            unc_date = unc_sid_dates[i]
-            mesh_file = unc_date.strftime(f'{self.mesh_dir}/%Y/mesh_%Y%m%d.zip')
-            unc_file = unc_date.strftime(f'{self.unc_dir}/%Y/unc_%Y%m%d.zip')
-
-            mf = MeshFile(unc_file)
-            # load precomputed integared uncertainty, skip the rest of processing
-            if unc_sid_name in get_file_arrays(unc_file):
-                unc_sid_int = mf.load([unc_sid_name], as_dict=False)[0]
-                continue
-
-            # load smearing uncertainty
-            unc_sid = mf.load(['unc_sid'], as_dict=False)[0]
-            if unc_sid_int is None:
-                # first time step: initialize integrated uncertainty
-                unc_sid_int = np.array(unc_sid)
-            else:
-                # later steps: advect unc_sid_int from previous time step
-                src2dst, weights = MeshFile(mesh_file).load(['src2dst', 'weights'], as_dict=False)
-                unc_sid_int_advected = np.zeros(src2dst[:,1].max()+1)
-                np.add.at(unc_sid_int_advected, src2dst[:,1], unc_sid_int[src2dst[:,0]] * weights)
-                # compute new integrated uncertainty after advection
-                unc_sid_int = np.hypot(unc_sid, unc_sid_int_advected)
-            
-            # Save integrated uncertainty
-            MeshFile(unc_file).save({unc_sid_name: unc_sid_int.astype(np.float16)}, mode='a')
-        print('Done', start_date, self.idates[stop_idx-1])
 
 
 class ComputeAgeUncertainty:
